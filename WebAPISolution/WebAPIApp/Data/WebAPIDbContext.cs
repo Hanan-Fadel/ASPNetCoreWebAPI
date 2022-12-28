bc3acba4d742afe197d6bd6ffbb21f26db9afa0c
Hanan Fadel <fadel_hanan@yahoo.com>
2022-12-28 15:20:19 -0700

    Create Domain Models, install EF Core, create our DBContext & inject it into the services collection, and create the database using migrations.

---
+++ WebAPISolution/WebAPIApp/Data/WebAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIApp.Models.Domain;
+
+namespace WebAPIApp.Data
+{
+    public class WebAPIDbContext : DbContext
+    {
+        public WebAPIDbContext(DbContextOptions<WebAPIDbContext> options) : base(options)
+        {
+
+        }
+
+        //Create the tables in the database if they are not exist
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
--- WebAPISolution/WebAPIApp/Program.cs
+++ WebAPISolution/WebAPIApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPIApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Inject our DbContext Class (WebAPIDbContext) into the Services collection
+builder.Services.AddDbContext<WebAPIDbContext>(options =>
+{
+    //get the connection string from the configuration file appsettings.json using builder.Configuration.GetConnectionString
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WalksDbConn"));
+});
 
 var app = builder.Build();
 
